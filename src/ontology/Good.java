be9b912f0ffb1a37e71e4e0bbc4509fb9b021b6a
Luis Gomes <lfcgomes@gmail.com>
2013-05-26 22:16:37 +0100

    Adicionada a class Good

---
+++ src/ontology/Good.java
@@ -0,0 +1,26 @@
+package ontology;
+public class Good {
+	private String name;
+	private int price;
+	
+	public Good(String name, int price){
+		this.name = name;
+		this.price = price;
+	}
+	
+	public int getPrice(){
+		return price;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setResPrice(int price){
+		this.price = price;
+	}
+}
