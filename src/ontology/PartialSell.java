9b07ca27be9c3e9fce8b6a33c8bc9efb1f2783c3
Luis Gomes <lfcgomes@gmail.com>
2013-06-04 16:39:03 +0100

    multi unit implemented

---
+++ src/ontology/PartialSell.java
@@ -0,0 +1,41 @@
+package ontology;
+
+import jade.content.AgentAction;
+
+public class PartialSell implements AgentAction {
+	private String goodname;
+	private int goodprice;
+	private int goodreserveprice;
+	private int qty;
+	
+	public int getQty()
+	{
+		return qty;
+	}
+	public void setQty(int newqty){
+		this.qty = newqty;
+	}
+	
+	public String getGoodName(){
+		return goodname;
+	}
+	
+	public void setGoodName(String goodname){
+		this.goodname = goodname;
+	}
+	
+	public int getGoodPrice() {
+		return this.goodprice;
+	}
+	public void setGoodPrice(int goodprice) {
+		this.goodprice = goodprice;
+	}
+
+	public int getGoodReservePrice() {
+		return goodreserveprice;
+	}
+
+	public void setGoodReservePrice(int goodreserveprice) {
+		this.goodreserveprice = goodreserveprice;
+	}
+}
